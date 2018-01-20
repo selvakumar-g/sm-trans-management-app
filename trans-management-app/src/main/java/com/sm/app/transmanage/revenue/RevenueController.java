a0208aa15bf95d9f04229c5021c5bc51ae612c5c
U811403 <U811403@WS17164192-CGO.ads.dlh.de>
2018-01-20 21:14:37 +0100

    initiated revenue module controller and service

--- trans-management-app/src/main/java/com/sm/app/transmanage/revenue/RevenueController.java
+++ trans-management-app/src/main/java/com/sm/app/transmanage/revenue/RevenueController.java
@@ -1,5 +1,8 @@
 package com.sm.app.transmanage.revenue;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RevenueController {
 
 }
--- trans-management-app/src/main/java/com/sm/app/transmanage/revenue/business/RevenueService.java
+++ trans-management-app/src/main/java/com/sm/app/transmanage/revenue/business/RevenueService.java
@@ -1,5 +1,34 @@
 package com.sm.app.transmanage.revenue.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sm.app.transmanage.loan.business.LoanService;
+import com.sm.app.transmanage.loantransaction.business.LoanTransactionService;
+import com.sm.app.transmanage.util.DozerMapper;
+import com.sm.app.transmanage.vehicle.business.VehicleService;
+import com.sm.app.transmanage.vehicletransaction.business.VehicleTransactionService;
+
+/**
+ * @author U811403
+ *
+ */
+@Service
 public class RevenueService {
+
+	@Autowired
+	DozerMapper dozerMapper;
+
+	@Autowired
+	LoanService loanService;
+
+	@Autowired
+	VehicleService vehicleService;
+
+	@Autowired
+	LoanTransactionService loanTxnService;
+
+	@Autowired
+	VehicleTransactionService vehicleTxnService;
 
 }
